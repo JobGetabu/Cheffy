b34b5c796e093aa92edb5b4e92345a5b6f3c8c63
JobGetabugit <getabujob@gmail.com>
2019-11-14 12:06:03 +0300

    #8 retrofit helper

---
+++ app/src/main/java/com/app/cheffyuser/networking/remote/RetrofitHelper.kt
@@ -0,0 +1,63 @@
+package com.app.cheffyuser.networking.remote
+
+import retrofit2.HttpException
+import java.net.SocketTimeoutException
+
+
+/*
+*
+* This helper retrofit class is used to make safe API calls from the server
+* with use of coroutines errors can be handled at one place.
+* useful info {@link href = https://github.com/JobGetabu/KoinExample }
+*
+* */
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+    companion object {
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String, data: T?): Resource<T> {
+            return Resource(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+    }
+}
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
+
+
+enum class ErrorCodes(val code: Int) {
+    SocketTimeOut(-1)
+}
+
+open class ResponseHandler {
+    fun <T : Any> handleSuccess(data: T): Resource<T> {
+        return Resource.success(data)
+    }
+
+    fun <T : Any> handleException(e: Exception): Resource<T> {
+        return when (e) {
+            is HttpException -> Resource.error(getErrorMessage(e.code()), null)
+            is SocketTimeoutException -> Resource.error(getErrorMessage(ErrorCodes.SocketTimeOut.code), null)
+            else -> Resource.error(getErrorMessage(Int.MAX_VALUE), null)
+        }
+    }
+
+    private fun getErrorMessage(code: Int): String {
+        return when (code) {
+            ErrorCodes.SocketTimeOut.code -> "Timeout"
+            401 -> "Unauthorised"
+            404 -> "Not found"
+            else -> "Something went wrong"
+        }
+    }
+}
