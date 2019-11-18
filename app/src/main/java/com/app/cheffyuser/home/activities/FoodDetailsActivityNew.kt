8979996d795898b13376a695a155aea7f4ff9a0f
JobGetabugit <getabujob@gmail.com>
2019-11-18 10:31:47 +0300

    #13 food details

--- app/src/main/java/com/app/cheffyuser/home/activities/FoodDetailsActivityNew.kt
+++ app/src/main/java/com/app/cheffyuser/home/activities/FoodDetailsActivityNew.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
-import androidx.viewpager.widget.ViewPager
 import com.app.cheffyuser.R
 import com.app.cheffyuser.home.adapter.Main3TabsAdapter
 import com.google.android.material.tabs.TabLayout
@@ -27,24 +26,23 @@ class FoodDetailsActivityNew : BaseActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
 
+        showSystemUI()
 
         tabLayout.addTab(tabLayout.newTab().setText("The Plate"))
         tabLayout.addTab(tabLayout.newTab().setText("Kitchen"))
         tabLayout.addTab(tabLayout.newTab().setText("Receipts"))
         tabLayout.tabGravity = TabLayout.GRAVITY_FILL
 
-
-        val viewPager = findViewById<View>(R.id.food_view_pager) as ViewPager
         val tabsAdapter = Main3TabsAdapter(supportFragmentManager, tabLayout.tabCount)
-        viewPager.adapter = tabsAdapter
-        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        viewpager.adapter = tabsAdapter
+        viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
         tabLayout.setOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
 
             override fun onTabSelected(tab: TabLayout.Tab) {
-                viewPager.currentItem = tab.position
+                viewpager.currentItem = tab.position
 
                 if (tab.position == 0) {
-                    top_im.setImageResource(R.drawable.details_background)
+                    top_im.setImageResource(R.drawable.upload_thumbnail)
                 }
 
                 if (tab.position == 1) {
@@ -64,9 +62,32 @@ class FoodDetailsActivityNew : BaseActivity() {
 
             }
         })
-
-
     }
 
 
+    /**
+     * TODO: Add focus change listener to show and hide the
+     *       bottom View cart entry
+     *
+     */
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        showSystemUI()
+    }
+
+    // Shows the system bars by removing all the flags
+    // except for the ones that make the content appear under the system bars.
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        showSystemUI()
+    }
 }
