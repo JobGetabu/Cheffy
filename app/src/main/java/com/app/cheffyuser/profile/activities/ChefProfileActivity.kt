bac47b238021d36a9928cdfb1793af51f31f6697
JobGetabugit <getabujob@gmail.com>
2019-11-19 09:13:05 +0300

    added and designed ChefProfileActivity #13 #9

---
+++ app/src/main/java/com/app/cheffyuser/profile/activities/ChefProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.app.cheffyuser.profile.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.cheffyuser.R
+
+class ChefProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chef_profile)
+    }
+}
