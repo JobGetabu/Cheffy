ff48719937119cec2edff7ab3e1b2bdad347396c
JobGetabugit <getabujob@gmail.com>
2019-12-03 22:28:10 +0300

    #9 added custom curved bottom nav

---
+++ app/src/main/java/com/app/cheffyuser/utils/RoundedBottomSheetDialogFragment.kt
@@ -0,0 +1,20 @@
+package com.app.cheffyuser.utils
+
+import android.app.Dialog
+import android.os.Bundle
+import com.app.cheffyuser.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ * BottomSheetDialog fragment that uses a custom
+ * theme which sets a rounded background to the dialog
+ * and doesn't dim the navigation bar
+ */
+open class RoundedBottomSheetDialogFragment : BottomSheetDialogFragment() {
+
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), theme)
+
+}
