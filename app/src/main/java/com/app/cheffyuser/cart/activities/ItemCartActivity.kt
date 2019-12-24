a8403d702f6bf5e659b54b42152508f97c9bbed4
JobGetabugit <getabujob@gmail.com>
2019-12-24 17:57:35 +0300

    #31 checkout UI

--- app/src/main/java/com/app/cheffyuser/cart/activities/ItemCartActivity.kt
+++ app/src/main/java/com/app/cheffyuser/cart/activities/ItemCartActivity.kt
@@ -3,14 +3,16 @@ package com.app.cheffyuser.cart.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.ViewModelProviders
 import com.app.cheffyuser.R
-import com.app.cheffyuser.cart.adapter.ItemCartAdapter
+import com.app.cheffyuser.create_account.model.ShippingData
 import com.app.cheffyuser.home.activities.BaseActivity
+import com.app.cheffyuser.home.viewmodel.HomeViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_item_cart.*
@@ -21,62 +23,131 @@ class ItemCartActivity : BaseActivity(), OnMapReadyCallback {
         fun newIntent(context: Context): Intent =
             Intent(context, ItemCartActivity::class.java)
 
+        const val DELIVERY = 0
+        const val PICKUP = 1
+        var SELECTDELIVERY = -1
+
+        const val CREDITCARD = 0
+        const val CASH = 1
+        var SELECTPAYMENT = -1
     }
 
-    internal var foodItemList = arrayOf("Grilled salmon", "Pasta Ham")
-    internal var foodPriceList = doubleArrayOf(96.00, 120.00)
-    internal var imgList = intArrayOf(R.drawable.img_food_1, R.drawable.img_food_2)
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var map: GoogleMap
+    private var ship: ShippingData? = null
 
-    private var mMap: GoogleMap? = null
+    private val vm: HomeViewModel by lazy {
+        ViewModelProviders.of(this).get(HomeViewModel::class.java)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_cart)
 
+        uiStuff()
 
-        // set a LinearLayoutManager with default vertical orientation
-        val linearLayoutManager = LinearLayoutManager(this@ItemCartActivity)
+    }
 
-        // set a LinearLayoutManager with default Horizontal orientation
-        // LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false);
+    private fun uiStuff() {
+        mapFragment = (supportFragmentManager
+            .findFragmentById(R.id.mMap) as SupportMapFragment?)!!
+        mapFragment.getMapAsync(this)
 
-        recycler_view.layoutManager = linearLayoutManager
+        ship = tokenManager.shippingData
 
-        //  call the constructor of CustomAdapter to send the reference and data to Adapter
-        val customAdapter =
-            ItemCartAdapter(this@ItemCartActivity, foodItemList, foodPriceList, imgList)
-        recycler_view.adapter = customAdapter // set the Adapter to RecyclerView
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment!!.getMapAsync(this@ItemCartActivity)
-
-        add_card_lay.setOnClickListener {
-            val intent = Intent(this@ItemCartActivity, AddCardActivity::class.java)
-            startActivity(intent)
+        btn_checkout.setOnClickListener {
+            checkOut()
         }
 
+        selectDeliveryFun()
+        selectPayment()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        map = googleMap!!
+
+        if (ship != null) {
+            if (ship!!.lat != null) {
+                setupMarker(LatLng(ship?.lat!!.toDouble(), ship?.lon!!.toDouble()))
+            } else {
+                val loc = LatLng(40.714890, -73.992690)
+                setupMarker(loc)
+            }
+        }
+    }
+
+    private fun setupMarker(loc: LatLng) {
+        map.addMarker(
+            MarkerOptions()
+                .position(loc)
+                .title("You")
+        )
+
+        val position = CameraPosition.Builder()
+            .target(loc)
+            .zoom(16F)
+            .tilt(20.0F)
+            .build()
+
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(position))
+    }
+
+    private fun selectDeliveryFun() {
+
+        if (SELECTDELIVERY == 0) {
+            //logic
+        } else {
+
+        }
+
+        deliver_btn.setOnClickListener {
+            SELECTDELIVERY = 0
+            deliver_btn.background = getMyDrawable(R.drawable.btn_bg_default)
+            deliver_btn.setTextColor(getMyColor(R.color.white))
+
+            pickup_btn.background = getMyDrawable(R.drawable.btn_bg_border)
+            pickup_btn.setTextColor(getMyColor(R.color.realblack))
+        }
+        pickup_btn.setOnClickListener {
+            SELECTDELIVERY = 1
+            pickup_btn.background = getMyDrawable(R.drawable.btn_bg_default)
+            pickup_btn.setTextColor(getMyColor(R.color.white))
+
+            deliver_btn.background = getMyDrawable(R.drawable.btn_bg_border)
+            deliver_btn.setTextColor(getMyColor(R.color.realblack))
+        }
+    }
+
+    private fun selectPayment() {
+        if (SELECTPAYMENT == 0) {
+            //logic
+        } else {
+
+        }
+
+        card_m_btn.setOnClickListener {
+            SELECTPAYMENT = 0
+            card_m_btn.background = getMyDrawable(R.drawable.button_round_1)
+            card_m_btn_txt.setTextColor(getMyColor(R.color.white))
+
+            cash_m_btn.background = getMyDrawable(R.drawable.round_border2)
+            cash_m_btn_txt.setTextColor(getMyColor(R.color.realblack))
+        }
+
+        cash_m_btn.setOnClickListener {
+            SELECTPAYMENT = 1
+            cash_m_btn.background = getMyDrawable(R.drawable.button_round_1)
+            cash_m_btn_txt.setTextColor(getMyColor(R.color.white))
+
+            card_m_btn.background = getMyDrawable(R.drawable.round_border2)
+            card_m_btn_txt.setTextColor(getMyColor(R.color.realblack))
+        }
 
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    private fun checkOut() {
 
-        // Add a marker in Sydney and move the camera
-        val latLng = LatLng(38.907192, -77.036873)
-        mMap!!.addMarker(MarkerOptions().position(latLng))
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
-
-
-        //for normal map view
-        mMap!!.mapType = GoogleMap.MAP_TYPE_NORMAL
-
-        //for sattelite map view
-        // mMap.setMapType(googleMap.MAP_TYPE_SATELLITE);
-
-        //for add zoom in maps
-        val zoomLevel = 16.0f //This goes up to 21
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel))
     }
+
 }
